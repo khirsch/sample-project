5544e28c4b5a6c3c7fecf8af54df6926de00fb65
Katrina Hirsch <katrina.hirsch@gmail.com>
2018-05-29 20:19:05 -0700

    remove unused code

--- src/app/index.js
+++ src/app/index.js
@@ -1,5 +1,3 @@
 import '../style/app.scss';
 import $ from 'jquery';
 // require('imports-loader?window.jQuery=jquery!./node_modules/owl-carousel/owl-carousel/owl.carousel.js');
-
-$('.owl-carousel').hide();
